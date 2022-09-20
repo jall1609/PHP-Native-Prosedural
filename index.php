ed198c48acd9b1111f5b9962807fc79df00e811e
jall1609 <rijalfahmi724@gmail.com>
2022-09-20 10:38:24 +0700

    membuat card

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <header>
@@ -44,6 +45,21 @@
 </header>
 
 <body>
+    <div id="daftar-katalog">
+        <h3>Daftar Katalog</h3>
+        <hr>
+        <div class="col col-md-2 ">
+            <div class="card bg-info " style="width: 8rem;">
+                <a href="#" class="katalog">
+                    <img class="cardKatalog rounded-circle" src="image/2.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="text-center  text-light">Keyboard</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
     <div id="kumpulanScript">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
