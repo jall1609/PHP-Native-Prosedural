5b12273e7642443d070a33c236a6ef6ed24e86c6
jall1609 <rijalfahmi724@gmail.com>
2022-09-21 10:08:40 +0700

    memperbaiki halaman statis

--- oprKatalog.php
+++ oprKatalog.php
@@ -5,7 +5,7 @@ include 'header.php';
 <hr>
 <button type="button" class="btn btn-info my-3">Tambah Katalog</button>
 <table class="table">
-    <thead class="thead bg-info text-white">
+    <thead class="thead bg-info text-center text-white">
         <tr>
             <th scope="col">No.</th>
             <th scope="col" width=" 40%">Gambar</th>
@@ -17,10 +17,19 @@ include 'header.php';
         <?php
         for ($i = 0; $i < 4; $i++) { ?>
             <tr>
-                <td scope="row"><?= $i + 1; ?></td>
-                <td><img src="image/2.jpg" alt="" width="30%"></td>
-                <td>Keyboard</td>
-                <td><button class="btn btn-warning mr-3">Edit</button><button class="btn btn-danger">Hapus</button></td>
+                <td class="align-middle text-center"><?= $i + 1; ?></td>
+                <td class="">
+                    <div class="d-flex justify-content-center"><img src="image/2.jpg" alt="" width="30%"></div>
+                </td>
+                <td class="align-middle  text-center">
+                    Keyboard
+                </td>
+                <td class="align-middle ">
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-warning mr-3">Edit</button>
+                        <button class="btn btn-danger">Hapus</button>
+                    </div>
+                </td>
             </tr>
         <?php
         };
