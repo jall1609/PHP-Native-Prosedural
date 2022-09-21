e6870fc82f9315e39bc063837671a4a30eaec162
jall1609 <rijalfahmi724@gmail.com>
2022-09-21 10:29:14 +0700

    selesai membuat tambah katalog

--- tambahKatalog.php
+++ tambahKatalog.php
@@ -1,5 +1,6 @@
 <?php
 include 'header.php';
+require 'koneksi.php';
 ?>
 <div class="container mt-5">
     <form action="" method="post" enctype="multipart/form-data">
@@ -19,4 +20,57 @@ include 'header.php';
 </div>
 <?php
 include 'footer.php';
+?>
+
+<?php
+if (isset($_POST['submit'])) {
+
+
+    function upload()
+    {
+        global $conn;
+        $namaKatalog = htmlspecialchars($_POST['namaKatalog']);
+
+        $namaFile = $_FILES['gambar']['name'];
+        $ukuranFile = $_FILES['gambar']['size'];
+        $error = $_FILES['gambar']['error'];
+        $tmpName = $_FILES['gambar']['tmp_name'];
+        if ($error == 4) {
+            echo "<script>alert('gambar harus diisi')</script>";
+            return false;
+        }
+
+        //mengecek extensi file yg di upload
+        $ekstensiYangDibolehkan = ['jpg', 'jpeg', 'png'];
+        $ekstensiGambar = explode('.', $namaFile);
+        $ekstensiGambar = strtolower(end($ekstensiGambar));
+        if (!in_array($ekstensiGambar, $ekstensiYangDibolehkan)) {
+            echo "<script>alert('format harus jpg, jpeg dan png')</script>";
+            return false;
+        }
+
+        //mengecek ukuran file
+        if ($ukuranFile > 1000000) {
+            echo "<script>alert('ukuran gambar max 1mb')</script>";
+            return false;
+        }
+        //merubah nama file
+        $namaFileBaru = uniqid() . '.' . $ekstensiGambar;
+
+        //input ke database
+        move_uploaded_file($tmpName, 'image/' .  $namaFileBaru);
+        // return $namaFileBaru;
+        if ($error == 0 and $namaKatalog != '') {
+            // echo $namaFileBaru . $namaKatalog;
+            $query = "INSERT INTO katalog VALUES ('', '$namaKatalog' , '$namaFileBaru')";
+            mysqli_query($conn, $query);
+            header('Location:oprKatalog.php');
+        }
+    }
+    $namaFileBaru = upload();
+    // print_r($_POST);
+    // print_r($_FILES);
+}
+
+
 ?>
